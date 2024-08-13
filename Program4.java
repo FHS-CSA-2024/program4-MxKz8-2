a452712ccd20b25a0e3c258e858c3cf4ff008c72
MxKz8-2 <kyletan555@gmail.com>
2024-08-13 09:57:40 -0700

    Update Program4.java
    Updated Completed Program 4

--- Program4.java
+++ Program4.java
@@ -11,6 +11,7 @@ public class Program4 {
         int c = 0;
         int d = 0;
         int sum = 0;
+        int avg = 0;
         
         System.out.println("Enter four three-digit-numbers.");
         System.out.print("Enter first number: ");
@@ -23,6 +24,7 @@ public class Program4 {
         d = myScanner.nextInt();
         
         sum = a + b + c + d;
+        avg = (a + b + c + d) / 4;
         System.out.println();
         System.out.println("The first number is: " + a);
         System.out.println("The second number is: " + b);
@@ -30,6 +32,8 @@ public class Program4 {
         System.out.println("The fourth number is: " + d);
         System.out.println();
         System.out.println("The sum is: " + sum);
+        System.out.println();
+        System.out.println("The average is: " + avg);
         System.out.println();
         System.out.println("This program was written by:");
         System.out.print("Kyle Tan on 8/13/24 in 1st period");
@@ -41,17 +45,19 @@ public class Program4 {
 /*
 
 Enter four three-digit-numbers.
-Enter first number: 123
-Enter second number: 456
-Enter third number: 789
-Enter fourth number: 101
+Enter first number: 475
+Enter second number: 821
+Enter third number: 369
+Enter fourth number: 562
 
-The first number is: 123
-The second number is: 456
-The third number is: 789
-The fourth number is: 101
+The first number is: 475
+The second number is: 821
+The third number is: 369
+The fourth number is: 562
 
-The sum is: 1469
+The sum is: 2227
+
+The average is: 556
 
 This program was written by:
 Kyle Tan on 8/13/24 in 1st period
